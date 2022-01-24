3300ac891653d30acf5a6f14a73b14c19288e65b
ferares <fermin@ares.uy>
2022-01-24 11:52:57 -0300

    Close the mobile menu on navigation

--- src/app/components/partials/header/header.component.ts
+++ src/app/components/partials/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -7,5 +8,7 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
   isCollapsed = true;
-  constructor() {}
+  constructor(private router: Router) {
+    router.events.subscribe(_ => this.isCollapsed = true);
+  }
 }
