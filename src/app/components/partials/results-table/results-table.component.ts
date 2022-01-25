209116fb519db773d117f57c3f387009bd34fd39
ferares <fermin@ares.uy>
2022-01-25 10:36:01 -0300

    Improve styles for results

--- src/app/components/partials/results-table/results-table.component.ts
+++ src/app/components/partials/results-table/results-table.component.ts
@@ -1,14 +1,19 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-results-table',
   templateUrl: './results-table.component.html',
   styleUrls: ['./results-table.component.scss']
 })
-export class ResultsTableComponent {
+export class ResultsTableComponent implements OnInit {
   @Input() results: Array<any> = undefined as any;
   @Input() origin = '';
   @Input() destination = '';
+  expanded: Array<Boolean> = [];
 
   constructor() { }
+
+  ngOnInit(): void {
+    this.expanded = this.results.map((_: any) => false);
+  }
 }
