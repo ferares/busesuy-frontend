8e6a4ca7245be4a7b50a72ea647ecaed313c8301
ferares <fermin@ares.uy>
2022-01-25 11:57:22 -0300

    Resolve locations & departments for lines in company pages

--- src/app/components/views/company/company.resolver.ts
+++ src/app/components/views/company/company.resolver.ts
@@ -18,7 +18,44 @@ export class CompanyResolver implements Resolve<any> {
         this.apiService.getLinesByCompany(name),
       ]).subscribe((data: Array<any>) => {
         const [company, lines] = data;
-        resolve({ company, lines })
+        const promises = [];
+        const locations: Array<any> = [];
+        for (const line of lines) {
+          if (!locations.includes(line.origin)) {
+            locations.push(line.origin);
+            promises.push(this.apiService.getLocationById(line.origin));
+          }
+          if (!locations.includes(line.destination)) {
+            locations.push(line.destination);
+            promises.push(this.apiService.getLocationById(line.destination));
+          }
+        }
+        combineLatest(promises).subscribe((locations: Array<any>) => {
+          const promises = [];
+          const departments: Array<any> = [];
+          for (const location of locations) {
+            if (!departments.includes(location.department)) {
+              departments.push(location.department);
+              promises.push(this.apiService.getDepartmentById(location.department));
+            }
+          }
+          combineLatest(promises).subscribe((departments: Array<any>) => {
+            for (const location of locations) {
+              location.department = departments.find(
+                department => department.id === location.department
+              ).name;
+            }
+            for (const line of lines) {
+              line.origin = locations.find(
+                location => location.id === line.origin
+              );
+              line.destination = locations.find(
+                location => location.id === line.destination
+              );
+            }
+            resolve({ company, lines });
+          });
+        });
       });
     });
   }
